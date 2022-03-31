28796f7231c4b2930403f773b870c2b9035ca40f
thedaebu <min.kim163@gmail.com>
2022-03-31 19:08:24 -0400

    3/31/22

---
+++ projects/java_project/src/java_project.java
@@ -0,0 +1,30 @@
+
+public class java_project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+		java_project.wassup();
+		
+		var example = new java_project();
+		example.ahnnyoung();
+		
+	}
+	
+	public static void wassup() {
+		System.out.println("wassup");
+	}
+	
+	public void ahnnyoung() {
+		System.out.println("ahnnyoung");
+	}
+
+}
+
+// classes have members
+// methods are members that do things
+// properties are members that store things
+
+// public/private - access modifier
+// static - states whether instance of class is needed
